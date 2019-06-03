abe8571b7e24cfa88eff65c151acf5659b9ea91e
ChuBoy <billju666@gmail.com>
2019-06-03 16:16:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ with open('README.md','r') as f:
     long_description = f.read()
 setuptools.setup(
     name = 'lazyplotly',
-    version = '0.1',
+    version = '1.2',
     author = 'chuboy',
     description = '',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/billju/lazyplotly',
-    #packages = setuptools.find_packages(),
-    scripts = ['lazyplotly.py']
+    #store project python in directory, and make an __init__.py to start
+    packages = setuptools.find_packages(),
 )
+#pipenv shell
+#pip install twine wheel
 #python setup.py sdist bdist_wheel
-#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+#twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
 #twine upload dist/*
